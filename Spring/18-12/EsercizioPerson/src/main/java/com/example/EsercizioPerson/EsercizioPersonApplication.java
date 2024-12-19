f3d229732666ae1d875f5300a9330e9a4a4fab22
Christian De Nicolo <126587728+Chris00Dot@users.noreply.github.com>
2024-12-19 13:47:46 +0100

    Esercitazioni
    Progetti svolti durante la lezione del docente

---
+++ Spring/18-12/EsercizioPerson/src/main/java/com/example/EsercizioPerson/EsercizioPersonApplication.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioPerson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioPersonApplication.class, args);
+	}
+
+}
