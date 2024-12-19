a41ee7f2d533a52216581367ce095b07f1681450
Christian De Nicolo <126587728+Chris00Dot@users.noreply.github.com>
2024-12-19 13:41:32 +0100

    Esercitazioni
    Esercitazioni svolte durante la lezione del docente

---
+++ Spring/18-12/EsercizioPerson/src/test/java/com/example/EsercizioPerson/EsercizioPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EsercizioPerson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsercizioPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
